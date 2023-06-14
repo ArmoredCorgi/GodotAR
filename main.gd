ef2665cfbd46e6520ab482ac1486957b234f2016
Nicholas Jorgensen <nicholas.jorgensen.vabgrk@statefarm.com>
2023-06-14 15:27:22 -0700

    changed ar interface to ar core

--- main.gd
+++ main.gd
@@ -1,14 +1,13 @@
 extends Node3D
 
-var openxr = null
+var ar_interface = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	openxr = XRServer.find_interface("OpenXR")
+	ar_interface = XRServer.find_interface("ARCore")
 	
-	if openxr and openxr.initialize():
+	if ar_interface and ar_interface.initialize():
 			get_viewport().use_xr = true
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
